897c10cc0a80ced61249de401344a22ba568f0f8
sammykagan <skagan@fwparker.org>
2019-02-11 10:38:35 -0600

    changed from 02_notes_formattingB.py to 02_notes_formatting.py

--- 03_hangman.py
+++ 03_hangman.py
@@ -71,15 +71,33 @@ HANGMANPICS = ['''
       |
 =========''']
 
+import random
 
+#Create the list of words and establish the word the player is trying to solve
+word_list = ["Lindsay", "FWPMUN", "Weekly", "Avani", "Colonel", "Klobuchar"]
+word = (word_list[random.randrange(0, len(word_list))])
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+available_letters = alphabet
 
+#Let's begin motherheckers
+print("\nWelcome to HANGMAN!\n")
+print("Here's your current board")
+print(HANGMANPICS[0])
+print("Here are the letters available to guess")
+print (available_letters)
+print("\nHere's the word you're guessing")
+for i in range (len(word)):
+    print ("__", end = " ")
+print ("\nIt's " + str(len(word)) + " letters long!")
 
+guess = input("\nGuess a letter!\n")
+available_letters.pop(available_letters.index(guess.upper()))
 
-
-
-
-
-
-
+if guess in word:
+    position = word.index(guess)
+    for i in range (len(position)):
+        print("__", end = " ")
+    print(guess)
+    for i in range (len(word - position))
 
 
