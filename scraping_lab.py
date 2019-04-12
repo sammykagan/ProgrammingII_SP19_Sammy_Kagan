ea81237c5f652477edbdb21324bf0631f9c9a43a
sammykagan <skagan@fwparker.org>
2019-04-12 18:57:33 -0400

    I'm done! Now you can get the weather without having to manually access that pesky Weather Channel.

--- scraping_lab.py
+++ scraping_lab.py
@@ -13,6 +13,11 @@
 # Note: Although it is possible to pull a description of the weather which includes much of this data, that is not the intent.
 # However, if you can do it and add the additional info, that works for me.
 
+# MY SOLE REMAINING ISSUE
+# On the first day, The Weather Channel seems to have taken out the value for a high temp. That is, the
+# high reads as "--/36" which messes with my program because the program splices at the "°" sign. Was today weird, or do
+# they always do this? Is it really so important that I go off and deal with it separately? I say no...
+
 from bs4 import BeautifulSoup
 import requests
 
@@ -22,24 +27,91 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.text, "html.parser")
 
+# DAYS
+day = soup.findAll(class_="date-time")
+day_list = []
+for j in day:
+    day_list.append(j.text)
+#print(day_list)
+
+
+# DATES
 date = soup.findAll(class_="day-detail clearfix")
-
 date_list = []
-
 for j in date:
     date_list.append(j.text)
+#print(date_list)
 
-print(date_list)
-'''
-all=soup.find("div",{"class":"locations-title ten-day-page-title"}).find("h1").text
+# DESCRIPTIONS
+description = soup.findAll(class_="description")
+desc_list = []
+for j in description:
+    desc_list.append(j.text)
+desc_list.pop(0)
+#print(desc_list)
 
-print(all)
+# HIGHS AND LOWS
+temperature = soup.findAll(class_="temp")
+temp_list = []
+for j in temperature:
+    temp_list.append(j.text)
+temp_list.pop(0)
+#print(temp_list)
 
-itema = soup.findAll("td",{"class":"description"})
-print(itema)
-'''
-temperature = soup.findAll(class_="temperature")
-print(temperature)
+#Build a list where the 'high' value and the 'low' value are separate items
+temp_list2 = []
+for k in temp_list:
+    temp_list2.append(k.split('°'))
+#print(temp_list2)
+
+# GET RID OF THAT BLASTED BLANK VALUE IN THE 2 SLOT
+# in retrospect, this was completely unnecessary
+temp_list3 = []
+for i in range(15):
+    temp_list3.append([])
+
+for i in range(len(temp_list2)):
+    temp_list3[i].append(temp_list2[i][0])
+    temp_list3[i].append(temp_list2[i][1])
+#print(temp_list3)
+
+# PRECIPITATION
+precipitation = soup.findAll(class_="precip")
+precip_list = []
+for j in precipitation:
+    precip_list.append(j.text)
+precip_list.pop(0)
+#print(precip_list)
+
+# WIND
+wind = soup.findAll(class_="wind")
+wind_list = []
+for j in wind:
+    wind_list.append(j.text)
+wind_list.pop(0)
+#print(wind_list)
+
+wind_list2 = []
+for k in wind_list:
+    wind_list2.append(k.split(' '))
+#print(wind_list2)
+
+# HUMIDITY
+humidity = soup.findAll(class_="humidity")
+humid_list = []
+for j in humidity:
+    humid_list.append(j.text)
+humid_list.pop(0)
+#print(humid_list)
+
+
+for i in range(10):
+    print("\n" + day_list[i] + ", " + date_list[i] + " will be " + desc_list[i].lower() + " with a high of " +
+          str(temp_list3[i][0]) + "° and a low of " + temp_list3[i][1] + "° as humidity sits at " + humid_list[i] +
+          ". Winds will come from the " + wind_list2[i][0] + " at " + wind_list2[i][1] + " mph and there is a " +
+          precip_list[i] + " chance of rain.")
+
+
 
 
 
